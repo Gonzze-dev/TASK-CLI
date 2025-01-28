91456107b5141073482c357b836f111f0472b6d9
Gonzalo E <errandoneagonzalo18@gmail.com>
2025-01-28 12:18:27 -0300

    create file if not exists

--- TASK-CLI/TaskRepository.cs
+++ TASK-CLI/TaskRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -11,11 +12,26 @@ namespace TASK_CLI
         private readonly string _path = "tasks.json";
         public List<Task> Tasks { get; set; }
         public TaskRepository() {
+            if (!File.Exists(_path))
+            {
+                string initialContent = "[]";
+                File.WriteAllText(_path, initialContent);
+            }
+
             Tasks = LoadData();
         }
         
         public Task? GetById(int id) => 
             Tasks.Find(task => task.Id == id);
+
+        public int GetLastId()
+        {
+            var task = Tasks.LastOrDefault();
+            var id = task == null ? 1 : task.Id + 1;
+
+            return id;
+        }
+           
 
         public IEnumerable<Task> ListBy(StatusTask status) =>
          Tasks.Where(task => task.Status == status);
@@ -48,6 +64,7 @@ namespace TASK_CLI
         
         void Save()
         {
+            
             var options = new JsonSerializerOptions
             {
                 Converters = { new EnumMemberConverter<StatusTask>() },
@@ -62,14 +79,28 @@ namespace TASK_CLI
         List<Task> LoadData()
         {
             string path = _path;
-            string jsonString = File.ReadAllText(path);
+            List<Task> tasks = [];
 
-            var options = new JsonSerializerOptions
+            try
             {
-                Converters = { new EnumMemberConverter<StatusTask>() }
-            };
+                string jsonString = File.ReadAllText(path);
 
-            var tasks = JsonSerializer.Deserialize<List<Task>>(jsonString, options) ?? [];
+                var options = new JsonSerializerOptions
+                {
+                    Converters = { new EnumMemberConverter<StatusTask>() },
+                    AllowTrailingCommas = true
+                };
+
+                tasks = JsonSerializer.Deserialize<List<Task>>(jsonString, options) ?? [];
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Could not find file");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
             return tasks;
         }
